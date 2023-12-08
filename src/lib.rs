a61a0474633025890c079c01e735d8b99d59155b
Chris Ktenidis <chris.ktenidis@csiro.au>
2023-12-08 14:59:23 +1000

    vertices, edges and graphs working

--- src/lib.rs
+++ src/lib.rs
@@ -20,28 +20,109 @@
 // }
 
 mod circuit_graph {
+    use std::collections::HashMap;
+
+    use petgraph::{prelude::*, data::Build, graph::Node};
+
+    #[derive(PartialEq, Eq)]
+    pub enum VertexType {
+        Source,
+        Sink,
+        Internal,
+    }
+
     pub struct VertexMetadata<T> {
         pub voltage: T,
+        pub tag: u32,
+        pub vertex_type: VertexType,
     }
 
     impl<T> VertexMetadata<T> {
-        pub fn new(voltage: T) -> VertexMetadata<T> {
-            VertexMetadata { voltage }
+        pub fn new(voltage: T, tag: u32, vertex_type: VertexType) -> Self {
+            Self {
+                voltage,
+                tag,
+                vertex_type,
+            }
+        }
+    }
+
+    pub struct EdgeMetadata<T> {
+        pub tail: u32,
+        pub head: u32,
+        pub conductance: T,
+    }
+
+    impl<T> EdgeMetadata<T> {
+        pub fn new(tail: u32, head: u32, conductance: T) -> Self {
+            Self { tail, head, conductance }
+        }
+    }
+
+    pub struct Circuit<T> {
+        pub graph: DiGraph<VertexMetadata<T>, EdgeMetadata<T>>,
+    }
+
+    impl<T> Circuit<T> {
+        pub fn new(vertices: Vec<VertexMetadata<T>>, edges: Vec<EdgeMetadata<T>>) -> Self {
+            let mut graph: DiGraph<VertexMetadata<T>, EdgeMetadata<T>> = DiGraph::new();
+
+            let mut vertex_indices: HashMap<u32, NodeIndex> = HashMap::new();
+            
+            for vertex in vertices {
+                let tag = vertex.tag.clone();
+                let node_index = graph.add_node(vertex);
+                vertex_indices.insert(tag, node_index);
+            }
+
+            for edge in edges {
+                graph.add_edge(
+                    *vertex_indices.get(&edge.tail).unwrap(),
+                    *vertex_indices.get(&edge.head).unwrap(),
+                    edge
+                );
+            }
+
+
+            Circuit { graph }
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use circuit_graph::VertexMetadata;
+    use petgraph::visit::IntoNodeIdentifiers;
 
-    use crate::circuit_graph;
+    use crate::circuit_graph::*;
 
     #[test]
     fn create_single_vertex() {
-        let v: VertexMetadata<f64> = VertexMetadata::new(1f64);
+        let v: VertexMetadata<f64> = VertexMetadata::new(1f64, 0, VertexType::Source);
 
         assert!(v.voltage == 1f64);
+        assert!(v.tag == 0);
+        assert!(v.vertex_type == VertexType::Source);
+    }
+
+    #[test]
+    fn create_single_edge() {
+        let e: EdgeMetadata<f64> = EdgeMetadata::new(0, 1, 0.5);
+
+        assert!(e.tail == 0);
+        assert!(e.head == 1);
+        assert!(e.conductance == 0.5);
+    }
+
+    #[test]
+    fn create_graph() {
+        let v1 = VertexMetadata::new(1.0, 1, VertexType::Source);
+        let v2 = VertexMetadata::new(0.0, 2, VertexType::Sink);
+
+        let e = EdgeMetadata::new(1, 2, 2.0);
+
+        let circuit = Circuit::new(vec![v1, v2], vec![e]);
+
+        assert!(circuit.graph.node_count() == 2);
     }
 
     /// Setup a simple circuit:
